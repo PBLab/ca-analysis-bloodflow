750a070038638d23a6dc3fdfbcbfafc8c4499a7e
hagai <hagaihargil@gmail.com>
2018-08-30 15:13:52 +0300

    Unique filtering of colabeled cells

--- ca_analysis/find_colabeled_cells.py
+++ ca_analysis/find_colabeled_cells.py
@@ -9,6 +9,7 @@ from attr.validators import instance_of
 import tifffile
 import scipy.ndimage
 import scipy.spatial.distance
+import scipy.stats
 import warnings
 import skimage.draw, skimage.measure
 
@@ -64,9 +65,11 @@ class ColabeledCells:
             self._show_images()
         self.struct_element = self._create_mask(self.cell_radius)
         region_props = self._find_cells(self.morph_img, self.struct_element)
-        func_idx, morph_idx = self._filter_regions(region_props)
-        unique_func_idx, unique_morph_idx = self._find_unique_pairs(func_idx, morph_idx)
-
+        dist_mat, func_idx, morph_idx = self._filter_regions(region_props)
+        min_distances = self._find_unique_pairs(dist_mat, func_idx, morph_idx)
+        if self.verbose:
+            self._show_colabeled_cells(min_distances)
+        
     def _show_images(self):
         """ Show the summed images of each channel of data """
         fig, ax = plt.subplots(1, 2)
@@ -83,7 +86,7 @@ class ColabeledCells:
         return mask
 
     def _find_cells(self, img, mask):
-        """ Finds cell-like shapes in an xy image """
+        """ Detects cell-like shapes in an xy image """
         assert len(img.shape) == 2
         quantile_val = np.percentile(img, 99.2)
         binary_img = np.zeros_like(img)
@@ -99,6 +102,7 @@ class ColabeledCells:
     def _filter_regions(self, regions):
         """ Filter each region in the labeled image by its area and location.
         Returns indices of the components that are close to each other. """
+
         # Start by reading the center of mass coordinates of CaImAn components
         all_crd = np.load(self.result_file)['crd']
         centroids_functional = np.array([data['CoM'] for data in all_crd])
@@ -106,34 +110,46 @@ class ColabeledCells:
         large_regions = [region for region in regions if region.area > self.cell_radius ** 2]
         centroids_morph = np.array([region.centroid for region in large_regions])
         assert centroids_morph.shape[1] == 2
+
+        # Calculate pair-wise distance of all cells, and find the closest ones
         dist = scipy.spatial.distance.cdist(centroids_functional, centroids_morph)
-        close_functional, close_morph = np.where(dist < 2 * self.cell_radius)
+        close_functional_idx, close_morph_idx = np.where(dist < 2 * self.cell_radius)
         if self.verbose:
             print("The distance filter reduced the number of detected morph cells"
-                  f" from {len(large_regions)} to {len(close_morph)}.")
-        return close_functional, close_morph
+                  f" from {len(large_regions)} to {len(np.unique(close_morph_idx))}.")
+        return dist, close_functional_idx, close_morph_idx
     
-    def _find_unique_pairs(self, func_idx, morph_idx):
-        """ Finds and returns only the unique pairs of morphological and
-        functional cells """
-        unique_func, unique_morph = [], []
-        for cur_func, cur_morph in zip(func_idx, morph_idx):
-            if (cur_func not in unique_func) and (cur_morph not in unique_morph):
-                unique_func.append(cur_func)
-                unique_morph.append(cur_morph)
+    def _find_unique_pairs(self, dist, func_idx, morph_idx):
+        """
+        Finds and returns only the unique pairs of morphological and
+        functional cells. The matrix it returns has the functional indices in column 0,
+        morphological indices in column 1, and the paired distance in column 2.
+        """        
+        dist_mat = np.full((min(len(np.unique(func_idx)), len(np.unique(morph_idx))), 3), np.nan)
+        dist_idx = 0
+        for func_i, morph_i in zip(func_idx, morph_idx):
+            if self.verbose:
+                print(f"dist_idx is {dist_idx}")
+            if (func_i not in dist_mat[:, 0]) and (morph_i not in dist_mat[:, 1]):
+                dist_mat[dist_idx, :] = (func_i, morph_i, dist[func_i, morph_i])
+                dist_idx += 1
                 continue
-            try:
-                idx = unique_func.index(cur_func)
-
+            if func_i in dist_mat[:, 0]:
+                dupe_idx = np.where(dist_mat[:, 0] == func_i)[0][0]
+                new_dist = dist[func_i, morph_i]
+                if new_dist < dist_mat[dupe_idx, 2]:
+                    dist_mat[dupe_idx, :] = (func_i, morph_i, new_dist)
+                continue
             
+            if morph_i in dist_mat[:, 1]:
+                dupe_idx = np.where(dist_mat[:, 1] == morph_i)[0][0]
+                new_dist = dist[func_i, morph_i]
+                if new_dist < dist_mat[dupe_idx, 2]:
+                    dist_mat[dupe_idx, :] = (func_i, morph_i, new_dist)
+
+        return dist_mat
 
 
-
-
-
-
-
-        
 if __name__ == '__main__':
     tif = pathlib.Path.home() / pathlib.Path(r'data/Amos/occluder/4th_July18_VIP_Td_SynGCaMP_Occluder/fov1_mag_2p5_256PX_58p28HZ_vasc_occ_00001.tif')
     result = pathlib.Path.home() / pathlib.Path(r'data/Amos/occluder/4th_July18_VIP_Td_SynGCaMP_Occluder/fov1_mag_2p5_256PX_58p28HZ_vasc_occ_00001_CHANNEL_1_results.npz')
