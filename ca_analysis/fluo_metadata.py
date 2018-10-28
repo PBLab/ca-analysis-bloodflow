7db65bdfca875d58a0d0043380faad4f66c06973
hagai <hagaihargil@gmail.com>
2018-10-28 08:52:24 +0200

    REFACTOR: fluo metadata

--- ca_analysis/fluo_metadata.py
+++ ca_analysis/fluo_metadata.py
@@ -16,6 +16,9 @@ class FluoMetadata:
     fps = attr.ib(default=15.24)  # framerate
     num_of_channels = attr.ib(default=1)
     start_time = attr.ib(default=0)
+    id_reg = attr.ib(default=r'(^\d+?)_', validator=instance_of(str))
+    day_reg = attr.ib(default=r'_DAY.+?(\d+)_', validator=instance_of(str))
+    fov_reg = attr.ib(default=r'_FOV.+?(\d+)_', validator=instance_of(str))
     timestamps = attr.ib(init=False)
     mouse_id = attr.ib(init=False)
     condition = attr.ib(init=False)
@@ -23,6 +26,14 @@ class FluoMetadata:
     fov = attr.ib(init=False)
 
     def get_metadata(self):
+        self._get_si_meta()
+        self.mouse_id = str(self._get_meta_using_regex(self.id_reg))
+        self.day = int(self._get_meta_using_regex(self.day_reg))
+        self.fov = int(self._get_meta_using_regex(self.fov_reg))
+        self.condition = 'Hyper' if 'HYPER' in str(self.fname) else 'Hypo'
+    
+    def _get_si_meta(self):
+        """ Parse the metadata from the SI-generated file """
         try:
             with tifffile.TiffFile(str(self.fname)) as f:
                 si_meta = f.scanimage_metadata
@@ -33,19 +44,11 @@ class FluoMetadata:
                 self.timestamps = np.arange(length)/self.fps
         except TypeError:
             self.timestamps = None
-        
-        id_reg = re.compile(r'(^\d+?)_')
-        self.mouse_id = id_reg.findall(str(self.fname.name))[0]
-        self.condition = 'Hyper' if 'HYPER' in str(self.fname) else 'Hypo'
 
-        day_reg = re.compile(r'_DAY.+?(\d+)_')
+    def _get_meta_using_regex(self, reg: str):
+        """ Parse the given regex from the filename """
+        reg = re.compile(reg)
         try:
-            self.day = int(day_reg.findall(str(self.fname.name))[0])
+            return reg.findall(str(self.fname.name))[0]
         except IndexError:
-            self.day = 99
-
-        fov_reg = re.compile(r'_FOV.+?(\d+)_')
-        try:
-            self.fov = int(fov_reg.findall(str(self.fname.name))[0])
-        except IndexError:
-            self.fov = 99
+            return 999
