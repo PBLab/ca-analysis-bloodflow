55be06f8fecac52f4f38492dfbc01e277bcbf3f3
Hagai Har-Gil <=>
2021-02-28 13:42:17 +0200

    Chagning overlay GUI step 1

--- examples/side_by_side.py
+++ examples/side_by_side.py
@@ -1,9 +1,12 @@
 import pathlib
 import json
+from typing import Optional
 
 import matplotlib
-matplotlib.use('Agg')
-from magicgui import magicgui, event_loop
+
+matplotlib.use("TkAgg")
+from magicgui import magicgui
+from magicgui.widgets import RangeEdit
 import colorcet as cc
 import skimage.transform
 import skimage.exposure
@@ -13,17 +16,13 @@ import matplotlib.pyplot as plt
 
 from calcium_bflow_analysis.dff_analysis_and_plotting import plot_cells_and_traces
 
-# linux only
-CACHE_FOLDER = pathlib.Path.home() / pathlib.Path('.cache/ca_analysis_bloodflow')
-CACHE_FOLDER.mkdir(mode=0o777, parents=True, exist_ok=True)
-
 
 def write_to_cache(foldername, data: dict):
     if not foldername.exists():
         return
-    filename = foldername / 'overlay_channels.json'
+    filename = foldername / "overlay_channels.json"
     try:
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             json.dump(data, f)
     except (FileNotFoundError, PermissionError) as e:
         print(repr(e))
@@ -32,7 +31,7 @@ def write_to_cache(foldername, data: dict):
 def read_from_cache(foldername) -> dict:
     if not foldername.exists():
         return
-    filename = foldername / 'overlay_channels.json'
+    filename = foldername / "overlay_channels.json"
     data = None
     try:
         with open(filename) as f:
@@ -42,87 +41,136 @@ def read_from_cache(foldername) -> dict:
     return data
 
 
-def _find_start_end_frames(inp: str):
-    splitted = inp.split(',')
-    if len(splitted) == 1:
-        return slice(None, None)
-    try:
-        start = int(splitted[0])
-    except ValueError:
-        start = None
-    try:
-        end = int(splitted[1])
-    except ValueError:
-        end = None
-    return slice(start, end)
+def _verify_fnames(*args):
+    """Verify each given filename for existence. Raises ValueError if it
+    doesn't."""
+    for fname in args:
+        if fname:
+            if not fname.exists():
+                raise ValueError
 
 
 def _normalize_arrays(ch1: np.ndarray, ch2: np.ndarray):
     if ch1.shape == ch2.shape:
         return ch1, ch2
     if ch1.shape[0] < ch2.shape[0]:
-        ch2 = skimage.transform.resize(ch2, ch1.shape, anti_aliasing=True, preserve_range=True)
+        ch2 = skimage.transform.resize(
+            ch2, ch1.shape, anti_aliasing=True, preserve_range=True
+        )
     else:
-        ch1 = skimage.transform.resize(ch1, ch2.shape, anti_aliasing=True, preserve_range=True)
-    ch1 = skimage.exposure.rescale_intensity(ch1, out_range='int16').astype('int16')
-    ch2 = skimage.exposure.rescale_intensity(ch2, out_range='int16').astype('int16')
+        ch1 = skimage.transform.resize(
+            ch1, ch2.shape, anti_aliasing=True, preserve_range=True
+        )
+    ch1 = skimage.exposure.rescale_intensity(ch1, out_range="int16").astype("int16")
+    ch2 = skimage.exposure.rescale_intensity(ch2, out_range="int16").astype("int16")
     return ch1, ch2
 
 
-@magicgui(call_button="Show", layout="form", ch1_fname={'fixedWidth': 1000})
-def overlay_channels_and_show_traces(ch1_fname: str = ".tif", ch1_frames: str = "", ch2_fname: str = ".tif", ch2_frames: str = "", results_fname: str = ".npz", cell_radius: int = 6):
-    ch1_fname = pathlib.Path(ch1_fname)
-    if not ch1_fname.exists():
-        return "Channel 1 path doesn't exist"
-    ch2_fname = pathlib.Path(ch2_fname)
-    if not ch2_fname.exists():
-        return "Channel 2 path doesn't exist"
-    results_fname = pathlib.Path(results_fname)
-    if not results_fname.exists():
-        return "Results path doesn't exist"
-    ch1_slice = _find_start_end_frames(ch1_frames)
-    ch2_slice = _find_start_end_frames(ch2_frames)
-    write_to_cache(CACHE_FOLDER, {'ch1_fname': str(ch1_fname), 'ch1_frames': ch1_frames, 'ch2_fname': str(ch2_fname), 'ch2_frames': ch2_frames, 'results_fname': str(results_fname), 'cell_radius': cell_radius})
-    print("reading files")
-    ch1 = tifffile.imread(str(ch1_fname))[ch1_slice]
-    if ch1.ndim == 3:
-        ch1 = ch1.mean(axis=0)
-    ch2 = tifffile.imread(str(ch2_fname))[ch2_slice]
-    if ch2.ndim == 3:
-        ch2 = ch2.mean(axis=0)
+def _process_single_channel_data(fname: pathlib.Path, frames: slice) -> np.ndarray:
+    """Quick wrapper around basic IO"""
+    data = tifffile.imread(str(fname))[frames]
+    if data.ndim == 3:
+        data = data.mean(axis=0)
+    return data
+
+
+def combine_two_images(ch1: np.ndarray, ch2: np.ndarray):
+    """Used in case the GUI has to show an overlay of two channels"""
     ch1, ch2 = _normalize_arrays(ch1, ch2)
-    print("finished reading tiffs")
     im = ch1 * 0.5 + ch2 * 0.5
-    new_fname = ch1_fname.parent / ('combined_' + ch1_fname.stem + '_' + ch2_fname.stem + '.tif')
-    roi_fname = str(new_fname.parent / ('only_roi_' + ch1_fname.stem + '_' + ch2_fname.stem + '.tif'))
+    return im
+
+
+def two_channel_pipeline(ch1: np.ndarray, ch1_fname: pathlib.Path, ch2: np.ndarray, ch2_fname: pathlib.Path, fig):
+    new_fname = ch1_fname.parent / (
+        "combined_" + ch1_fname.stem + "_" + ch2_fname.stem + ".tif"
+    )
+    roi_fname = str(
+        new_fname.parent
+        / ("only_roi_" + ch1_fname.stem + "_" + ch2_fname.stem + ".tif")
+    )
     new_fname = str(new_fname)
     tifffile.imwrite(new_fname, np.stack([ch1, ch2]))
-    fig = plot_cells_and_traces.show_side_by_side([im], [results_fname], None, cell_radius)
-    plot_cells_and_traces.draw_rois_over_cells(im, cell_radius, results_file=results_fname, roi_fname=roi_fname)
     # ch1 -= ch1.min()
     # ch2 -= ch2.min()
     vmin1, vmax1 = ch1.min() * 1.1, ch1.max() * 0.9
     vmin2, vmax2 = ch2.min() * 1.1, ch2.max() * 0.9
     fig.axes[0].images.pop()
-    fig.axes[0].imshow(ch1, cmap=cc.cm.kg, vmin=vmin1, vmax=vmax1)
+    fig.axes[0].imshow(ch1, cmap=cc.cm.kg, alpha=0.55, vmin=vmin1, vmax=vmax1)
     fig.axes[0].imshow(ch2, cmap=cc.cm.kr, alpha=0.55, vmin=vmin2, vmax=vmax2)
-    fig.axes[0].set_title('Ch1 is green, Ch2 is red')
+    fig.axes[0].set_title("Ch1 is green, Ch2 is red")
     fig.canvas.set_window_title(f"{new_fname}")
     plt.show(block=False)
-    return new_fname
+    return roi_fname
 
 
-if __name__ == '__main__':
-    data = read_from_cache(CACHE_FOLDER)
-    with event_loop():
-        gui = overlay_channels_and_show_traces.Gui(show=True)
-        if data:
-            gui.ch1_fname = data['ch1_fname']
-            gui.ch2_fname = data['ch2_fname']
-            gui.results_fname = data['results_fname']
-            gui.cell_radius = data['cell_radius']
-            gui.ch1_frames = data['ch1_frames']
-            gui.ch2_frames = data['ch2_frames']
-        gui.called.connect(lambda x: gui.set_widget("Message:", str(x), position=-1))
+@magicgui(
+    call_button="Show",
+    persist=True,
+    result_widget=True,
+    main_window=True,
+    ch1_fname= {'label': 'Data file', 'filter': ['.tif', '.tiff']},
+    ch1_frames={'label': 'Relevant data frames', 'start': 0, 'stop': 40_000},
+    results_fname={'label': "CaImAn's HDF5", 'filter': ['.h5', '.hdf5']},
+    ch2_fname={'label': 'Overlay data with', 'filter': ['.tif', '.tiff']},
+    ch2_frames={'label': 'Relevant overlay frames', 'start': 0, 'stop': 40_000},
+)
+def show_traces_and_rois(
+    ch1_fname: pathlib.Path,
+    ch1_frames: RangeEdit,
+    results_fname: pathlib.Path,
+    ch2_fname: Optional[pathlib.Path],
+    ch2_frames: Optional[RangeEdit],
+):
+    """Shows calicum traces and cell ROIs with a possible overlay of a second
+    color.
+
+    This tool is meant to assist in viewing the results of the CaImAn pipeline
+    by showing a max projection of the data with the detected ROIs overlayed,
+    and with the calcium traces presented at its side. Additionally the user
+    may also provide a second color channel that will be overlayed on top of
+    the first if that is of help.
+
+    In addition to that, only specific frames from the recording can be
+    included if the data contains two channels which aren't interleaved, or in
+    other extraordinary cases.
+
+    Parameters
+    ----------
+    ch1_fname : pathlib.Path
+        Main data channel to use as the baseline image, usually the calcium
+        activity data
+    ch1_frames : napari.widgets.RangeEdit
+        Frame numbers to use (0-based)
+    results_fname : pathlib.Path
+        The CaImAn-generated HDF5 results file
+    ch2_fname : pathlib.Path, optional
+        A second color to overlay on top of the first, optional
+    ch2_frames : napari.widgets.RangeEdit
+        Frame numbers to use (0-based)
+    """
+    try:
+        _verify_fnames(ch1_fname, ch2_fname, results_fname)
+    except ValueError:
+        return "Filepath error, please re-try"
+    ch1 = _process_single_channel_data(ch1_fname, ch1_frames)
+    if ch2_fname:
+        ch2 = _process_single_channel_data(ch2_fname, ch2_frames)
+        im = combine_two_images(ch1, ch2)
+    else:
+        im = ch1
+    fig = plot_cells_and_traces.show_side_by_side(
+        [im], [results_fname], None
+    )
+    if ch2_fname:
+        roi_fname = two_channel_pipeline(ch1, ch1_fname, ch2, ch2_fname, fig)
+    else:
+        roi_fname = ch1_fname.parent / ("only_roi" + ch1_fname.name)
+    plot_cells_and_traces.draw_rois_over_cells(
+        im, results_file=results_fname, roi_fname=roi_fname
+    )
+    return roi_fname
 
 
+if __name__ == "__main__":
+    show_traces_and_rois.show(run=True)
